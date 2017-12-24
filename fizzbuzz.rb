537aabee376dec75ab0e2210209b6db0e00035fb
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-24 20:58:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@ def fizzbuzz(num)
   if num % 3 == 0
     return "Fizz"
 
-  elsif num % 5 == 0 && num % 3 !== 0
+  elsif num % 5 == 0 && num % 3 != 0
     return "Buzz"
 
   elsif (num % 5 == 0) && (num % 3 == 0)
